202a6f36aada8b3078beb20484fc3368f6e2fa4d
W0837901 <W0837901@ADL-A1201I-12>
2023-11-13 20:11:52 -0500

    update unt of work

--- MuhammedsBooks.DataAccess/Repository/UnitOfWork.cs
+++ MuhammedsBooks.DataAccess/Repository/UnitOfWork.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MuhammedsBooks.DataAccess.Repository.IRepository;
+using MuhammedsWebStore.DataAccess.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,25 @@ using System.Threading.Tasks;
 
 namespace MuhammedsBooks.DataAccess.Repository
 {
-    class UnitOfWork
+    public class UnitOfWork //making the method public
     {
+        private readonly ApplicationDbContext _db; //the using statment
+
+        public UnitOfWork(ApplicationDbContext db) //constructor to use DI and inject in the repositories
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+            SP_Call = new SP_Call(_db);
+        }
+        public ICategoryRepository Category { get; private set; }
+        public ISP_Call SP_Call { get; private set; }
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+        public void Save() //all changes will be saved when the save method is called at the parent level
+        {
+            _db.SaveChanges();
+        }
     }
 }
